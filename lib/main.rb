5b66bbc3c3afa123c1074b9cadff4f66bd592af8
Brandon O. Niles <b21niles@gmail.com>
2018-01-26 17:56:06 -0500

    switched to working on the terminal interface. created loop and menu screen

--- lib/main.rb
+++ lib/main.rb
@@ -1,7 +1,26 @@
 require_relative './pencil'
 
-x = Pencil.new(paper: 'test_paper', dura: 20)
-  puts x.dura
-  x.write(x.get_str)
-  puts x.paper
-  puts x.dura
+while true
+	puts "Welcome to Digi-Pencil:"
+  puts "1. Create a Pencil"
+	puts "2. Write"
+	puts "3. Erase"
+	puts "4. Edit"
+  puts "5. Pencil Stats"
+	puts "0. Exit Program"
+
+	input = gets.chomp
+
+	case input
+	when '1' then create_pencil
+	when '2' then curr_pencil.write
+	when '3' then curr_pencil.erase
+  when '4' then curr_pencil.edit
+  when '5' then curr_pencil.report
+	when '0' then abort
+	else
+	puts "unrecognizable command"
+
+	end
+
+end
