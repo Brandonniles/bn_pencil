1285c7b95c45d6d3ff506f88b074c10df9679fda
Brandon O. Niles <b21niles@gmail.com>
2018-01-26 00:24:50 -0500

    need to research 'exist' matcher. moved on and wrote test for a pencil to be associated witha piece of paper

--- lib/pencil.rb
+++ lib/pencil.rb
@@ -1,5 +1,5 @@
 class Pencil
-  
+
   def initialize()
 
   end
--- spec/pencil_spec.rb
+++ spec/pencil_spec.rb
@@ -1,13 +1,17 @@
 require_relative '../lib/pencil'
 
 RSpec.describe Pencil do
-  let(:pillar_pencil) { Pencil.new() }
+  let(:pillar_pencil) { Pencil.new(paper: 'sheet1') }
 
   it "creates a new pencil" do
     expect(pillar_pencil).to be_an_instance_of Pencil
   end
 
-  it "expects a pencil to have a write method" do
-    expect(pillar_pencil.write).to exist
+  it "a pencil will have paper" do
+    expect(pillar_pencil.paper).to eql('sheet1')
   end
+
+  # it "expects a pencil to have a write method" do
+  #   expect(pillar_pencil.write()).to exist
+  # end
 end
